90550fb541282c3e5820a7b1abacb99d7eb24fc4
AngieArlanti <angeles.arlanti@mercadolibre.com>
2019-08-24 22:25:41 -0300

    Beacon found with bridge again

--- android/app/src/main/java/com/pfexpobeacon/BeaconMonitorManager.java
+++ android/app/src/main/java/com/pfexpobeacon/BeaconMonitorManager.java
@@ -1,18 +1,27 @@
 package com.pfexpobeacon;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+
 import androidx.annotation.NonNull;
+
+import android.os.Build;
 import android.util.Log;
+import android.widget.Toast;
+
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.WritableMap;
+
 import java.util.Collection;
+
 import javax.annotation.Nonnull;
+
 import org.altbeacon.beacon.Beacon;
 import org.altbeacon.beacon.BeaconConsumer;
 import org.altbeacon.beacon.BeaconManager;
@@ -35,9 +44,13 @@ public class BeaconMonitorManager extends ReactContextBaseJavaModule implements 
     private BeaconManager beaconManager = null;
     private Region beaconRegion = null;
     private Promise promise;
+    private ReactApplicationContext reactContext;
+    private Context applicationContext;
 
     public BeaconMonitorManager(@Nonnull final ReactApplicationContext reactContext) {
         super(reactContext);
+        this.reactContext = reactContext;
+        this.applicationContext = this.reactContext.getApplicationContext();
     }
 
     @Nonnull
@@ -45,6 +58,22 @@ public class BeaconMonitorManager extends ReactContextBaseJavaModule implements 
     public String getName() {
         return "BeaconMonitor";
     }
+
+    private void requestPermissions() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            getCurrentActivity().requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,
+                            Manifest.permission.ACCESS_FINE_LOCATION}, 1234);
+        }
+    }
+
+    private void configureBeaconManager() {
+        //1. Fetch the beacon manager singleton for the app.
+        beaconManager = BeaconManager.getInstanceForApplication(applicationContext);
+        //2. Set the beacon manager a beacon Pazza with a set layout.
+        beaconManager.getBeaconParsers().add(new BeaconParser().
+                setBeaconLayout("m:0-3=4c000215,i:4-19,i:20-21,i:22-23,p:24-24"));
+    }
+
 
     /**
      * 7. Once you have set the service connect method you're in position to start actually monitoring for a
@@ -56,62 +85,10 @@ public class BeaconMonitorManager extends ReactContextBaseJavaModule implements 
     @ReactMethod
     public void startBeaconMonitoring(@NonNull final Promise promise) {
         Log.d(TAG, "startBeaconMonitoring called");
-        if (beaconManager != null && beaconManager.isBound(this) && beaconRegion != null) {
-            Log.d(TAG, "beaconManager and beaconRegion already exists");
-            return;
-        }
+
+        requestPermissions();
+
         configureBeaconManager();
-
-        try {
-            //Region(UniqueId, UUID, major, minor)
-            beaconRegion =
-                new Region("MyBeacons", null, null, null);
-
-            beaconManager.startMonitoringBeaconsInRegion(beaconRegion);
-            beaconManager.startRangingBeaconsInRegion(beaconRegion);
-            this.promise = promise;
-        } catch (Exception e) {
-            //Tell react native about the exception
-            promise.reject(BEACON_MANAGER_DID_NOT_START_ERROR, e);
-            e.printStackTrace();
-        }
-    }
-
-    @ReactMethod
-    public void stopBeaconMonitoring(@NonNull final Promise promise) {
-        Log.d(TAG, "stopBeaconMonitoring called");
-        if (beaconManager == null || beaconRegion == null) {
-            return;
-        }
-        try {
-            //beaconManager.stopMonitoringBeaconsInRegion(beaconRegion);
-            beaconManager.stopRangingBeaconsInRegion(beaconRegion);
-        } catch (Exception e) {
-            e.printStackTrace();
-            //Tell react native about the exception
-            promise.reject(BEACON_MANAGER_DID_NOT_STOP_ERROR, e);
-        }
-        beaconManager.removeAllRangeNotifiers();
-        beaconManager.removeAllMonitorNotifiers();
-        beaconManager.unbind(this);
-    }
-
-    private void configureBeaconManager() {
-        if (beaconManager != null && beaconManager.isBound(this)) {
-            Log.d(TAG, "beaconManager already exists");
-            return;
-        }
-
-        //1. Fetch the beacon manager singleton for the app.
-        beaconManager = BeaconManager.getInstanceForApplication(getApplicationContext());
-        //2. Set the beacon manager a beacon Pazza with a set layout.
-        //beaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout(BeaconParser.ALTBEACON_LAYOUT));
-        //beaconManager.getBeaconParsers().add(new BeaconParser(BeaconParser.EDDYSTONE_TLM_LAYOUT));
-        //beaconManager.getBeaconParsers().add(new BeaconParser(BeaconParser.EDDYSTONE_UID_LAYOUT));
-        //beaconManager.getBeaconParsers().add(new BeaconParser(BeaconParser.EDDYSTONE_URL_LAYOUT));
-        //beaconManager.getBeaconParsers().add(new BeaconParser(BeaconParser.URI_BEACON_LAYOUT));
-        beaconManager.getBeaconParsers().add(new BeaconParser().
-            setBeaconLayout("m:0-3=4c000215,i:4-19,i:20-21,i:22-23,p:24-24"));
 
         //3. Bind the manager to a class tha implements the Android Beacon library beacon consumer interface.
         //In order to do that we can implement our MainActivity to implement the beacon consumer interface.
@@ -119,6 +96,32 @@ public class BeaconMonitorManager extends ReactContextBaseJavaModule implements 
 
         beaconManager.setBackgroundMode(false);
         beaconManager.setForegroundBetweenScanPeriod(DEFAULT_FOREGROUND_SCAN_PERIOD);
+
+        try {
+            //Region(UniqueId, UUID, major, minor)
+            beaconRegion =
+                    new Region("MyBeacons", null, null, null);
+
+            beaconManager.startMonitoringBeaconsInRegion(beaconRegion);
+            beaconManager.startRangingBeaconsInRegion(beaconRegion);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @ReactMethod
+    public void stopBeaconMonitoring(@NonNull final Promise promise) {
+        Log.d(TAG, "stopBeaconMonitoring called");
+        try {
+            beaconManager.stopMonitoringBeaconsInRegion(beaconRegion);
+            beaconManager.stopRangingBeaconsInRegion(beaconRegion);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        beaconManager.removeAllRangeNotifiers();
+        beaconManager.removeAllMonitorNotifiers();
+        beaconManager.unbind(this);
     }
 
     @Override
@@ -151,9 +154,9 @@ public class BeaconMonitorManager extends ReactContextBaseJavaModule implements 
                     for (Beacon beacon : beacons) {
                         Log.d(TAG, "Beacon " + i + " found");
                         i++;
-                        //Return react native beacon Info
-                        //fixme promise.resolve() can be called just once. Array must be delivered.
-                        resolvePromise(beacon);
+                        showBeaconInfo(beacon);
+                        //FIXME pasar array
+                        //resolvePromise(beacon);
                     }
                 } else {
                     Log.d(TAG, "No beacons found.");
@@ -161,6 +164,44 @@ public class BeaconMonitorManager extends ReactContextBaseJavaModule implements 
             }
         });
     }
+
+    private void showBeaconInfo(final Beacon beacon) {
+        final String message = "Mac addr: " + beacon.getBluetoothAddress() + "Distance: " + beacon.getDistance();
+
+        Toast.makeText(applicationContext, message, Toast.LENGTH_SHORT).show();
+    }
+
+    /**
+     * FIXME array delivered
+     * private WritableMap createRangingResponse(Collection<Beacon> beacons, Region region) {
+     * WritableMap map = new WritableNativeMap();
+     * map.putString("identifier", region.getUniqueId());
+     * map.putString("uuid", region.getId1() != null ? region.getId1().toString() : "");
+     * WritableArray a = new WritableNativeArray();
+     * for (Beacon beacon : beacons) {
+     * WritableMap b = new WritableNativeMap();
+     * b.putString("uuid", beacon.getId1().toString());
+     * if (beacon.getIdentifiers().size() > 2) {
+     * b.putInt("major", beacon.getId2().toInt());
+     * b.putInt("minor", beacon.getId3().toInt());
+     * }
+     * b.putInt("rssi", beacon.getRssi());
+     * if(beacon.getDistance() == Double.POSITIVE_INFINITY
+     * || Double.isNaN(beacon.getDistance())
+     * || beacon.getDistance() == Double.NaN
+     * || beacon.getDistance() == Double.NEGATIVE_INFINITY){
+     * b.putDouble("distance", 999.0);
+     * b.putString("proximity", "far");
+     * }else {
+     * b.putDouble("distance", beacon.getDistance());
+     * b.putString("proximity", getProximity(beacon.getDistance()));
+     * }
+     * a.pushMap(b);
+     * }
+     * map.putArray("beacons", a);
+     * return map;
+     * }
+     */
 
     private void resolvePromise(@NonNull final Beacon beacon) {
         if (promise != null) {
@@ -173,37 +214,18 @@ public class BeaconMonitorManager extends ReactContextBaseJavaModule implements 
         }
     }
 
-    private void addMonitorNotifier() {
-        beaconManager.addMonitorNotifier(new MonitorNotifier() {
-            @Override
-            public void didEnterRegion(final Region region) {
-
-            }
-
-            @Override
-            public void didExitRegion(final Region region) {
-
-            }
-
-            @Override
-            public void didDetermineStateForRegion(final int i, final Region region) {
-                /* Not implemented */
-            }
-        });
-    }
-
     @Override
     public Context getApplicationContext() {
-        return getReactApplicationContext();
+        return applicationContext;
     }
 
     @Override
-    public void unbindService(final ServiceConnection serviceConnection) {
-
+    public void unbindService(ServiceConnection serviceConnection) {
+        applicationContext.unbindService(serviceConnection);
     }
 
     @Override
-    public boolean bindService(final Intent intent, final ServiceConnection serviceConnection, final int i) {
-        return false;
+    public boolean bindService(Intent intent, ServiceConnection serviceConnection, int i) {
+        return applicationContext.bindService(intent, serviceConnection, i);
     }
 }
