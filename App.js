faa0178770c38454c111bd534e46b395349c9ce7
AngieArlanti <angeles.arlanti@mercadolibre.com>
2019-08-31 17:26:54 -0300

    sample comment

--- App.js
+++ App.js
@@ -42,6 +42,27 @@ suscribeForEvents() {
   this.subscription = DeviceEventEmitter.addListener('didRangeBeaconsInRegion', (data) => {
     //TODO abrir pantalla con los beacons listados. 
     //Podes usar este this.state.isDataAvailable y this.state.data para mostrar lista de beacons en el render().
+    /* Vas a recibir esto:
+    "beacons":[  
+         {  
+            "proximity":"immediate",
+            "distance":0.01009895532367115,
+            "uuid":"2f234454-cf6d-4a0f-adf2-f4911ba9ffa6",
+            "major":0,
+            "minor":1,
+            "rssi":-48,
+            "macAddress":"0C:F3:EE:08:FC:DD"
+         },
+         {  
+            "proximity":"immediate",
+            "distance":0.11185681527500883,
+            "uuid":"2f234454-cf6d-4a0f-adf2-f4911ba9ffa6",
+            "major":0,
+            "minor":1,
+            "rssi":-49,
+            "macAddress":"0C:F3:EE:04:19:21"
+         }
+    */
     this.state = { isDataAvailable: true};
     this.state = { data: data.beacons};
     ToastAndroid.show("Beacons: " + data.beacons[0].macAddress, ToastAndroid.SHORT);
