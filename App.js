3b051edd109a1dbbd3a470e87825448fe066834f
Mauricio Minestrelli <mauricio.minestrelli@mercadolibre.com>
2019-10-14 20:21:35 -0300

    - Fetching data from to render a cell in stand detail

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ constructor(props) {
   this.state = { buttonText: isOffText};
   this.state = { isDataAvailable: false};
   this.state = { data: [
-            {macAddress: 'Devin'},
+            {macAddress: '0C:F3:EE:04:18:A0'},
             {macAddress: 'Dan'},
             {macAddress: 'Dominic'},
             {macAddress: 'Jackson'},
@@ -31,7 +31,7 @@ constructor(props) {
 _onStatusChange = e => {
   if(this.state.isOn){
     console.log("stop");
-    this.stopRangingBeacons();  
+    this.stopRangingBeacons();
     this.setState({ buttonText: isOffText});
     this.setState({ isOn: false});
   }else {
@@ -53,11 +53,11 @@ startRangingBeacons() {
 
 suscribeForEvents() {
   this.startSubscription = DeviceEventEmitter.addListener(BeaconManager.EVENT_BEACONS_RANGED, (data) => {
-    //TODO abrir pantalla con los beacons listados. 
+    //TODO abrir pantalla con los beacons listados.
     //Podes usar este this.state.isDataAvailable y this.state.data para mostrar lista de beacons en el render().
     /* Vas a recibir esto:
-    "beacons":[  
-         {  
+    "beacons":[
+         {
             "proximity":"immediate",
             "distance":0.01009895532367115,
             "uuid":"2f234454-cf6d-4a0f-adf2-f4911ba9ffa6",
@@ -66,7 +66,7 @@ suscribeForEvents() {
             "rssi":-48,
             "macAddress":"0C:F3:EE:08:FC:DD"
          },
-         {  
+         {
             "proximity":"immediate",
             "distance":0.11185681527500883,
             "uuid":"2f234454-cf6d-4a0f-adf2-f4911ba9ffa6",
@@ -81,7 +81,7 @@ suscribeForEvents() {
       data: data.beacons
     });
     ToastAndroid.show("Beacons: " + data.beacons[0].macAddress, ToastAndroid.SHORT);
-  })  
+  })
 }
 
 
@@ -127,8 +127,6 @@ backgroundColor: '#F5FCFF',
 },
 top: {
 flex: 1,
-alignItems: "center",
-justifyContent: "center",
 },
 bottom: {
 flex: 1,
--- standList.js
+++ standList.js
@@ -1,21 +1,72 @@
 import React, { Component } from 'react';
-import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View,Image,ScrollView } from 'react-native';
+import { ListItem } from 'react-native-elements'
 
 /**
 * @param props properties needed to render StandList:
 * - stands: array of stands' data.
 */
 export default class StandList extends React.Component {
+  constructor(props){
+  super(props);
+  this.state ={ isLoading: true};
+  this.state = { isDataAvailable: false};
+}
+  componentDidMount(){
+    this.getStands();
+  }
+  getStands(){
+    console.log('---------ESTO NO SON PATRANAS----------'+'http://private-f63ff-standsv1.apiary-mock.com/stands/'+this.props.stands[0].macAddress);
+    return fetch('http://private-f63ff-standsv1.apiary-mock.com/stands/'+this.props.stands[0].macAddress)
+      .then((response) => response.json())
+      .then((responseJson) => {
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <FlatList
-          data={this.props.stands}
-          renderItem={({item}) => <Text style={styles.item}>{item.macAddress}</Text>}
-        />
-      </View>
-    );
+        this.setState({
+          isLoading: false,
+          dataSource: responseJson,
+        }, function(){
+
+        });
+
+      })
+      .catch((error) =>{
+        console.error(error);
+      });
+  }
+  keyExtractor = (item, index) => index.toString()
+
+  renderItem = ({ item }) => (
+  <ListItem
+    title={item.title}
+    subtitle={item.description}
+    leftAvatar={{
+      source: item.picture && { uri: item.picture }
+    }}
+    bottomDivider
+    chevron
+  />
+  )
+
+  render () {
+  return (
+    <View style={styles.container}>
+    <ScrollView
+        style={styles.container}
+        contentContainerStyle={styles.contentContainer}>
+    <View style={styles.container}>
+      <FlatList
+        data={this.props.stands}
+        renderItem={({item}) => <Text style={styles.item}>{item.macAddress}</Text>}
+      />
+    </View>
+    <FlatList
+      keyExtractor={this.keyExtractor}
+      data={this.state.dataSource}
+      renderItem={this.renderItem}
+    />
+    </ScrollView>
+  </View>
+  )
   }
 }
 
@@ -28,5 +79,26 @@ const styles = StyleSheet.create({
     padding: 10,
     fontSize: 18,
     height: 44,
+  },
+  tabBarInfoText: {
+  fontSize: 17,
+  color: 'rgba(96,100,109, 1)',
+  textAlign: 'center',
+  },
+  beaconDetailContainer: {
+    marginTop: 15,
+    ...Platform.select({
+      ios: {
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: -3 },
+        shadowOpacity: 0.1,
+        shadowRadius: 3,
+      },
+      android: {
+        elevation: 20,
+      },
+    }),
+    alignItems: 'center',
+    backgroundColor: '#fbfbfb',
   },
 })
